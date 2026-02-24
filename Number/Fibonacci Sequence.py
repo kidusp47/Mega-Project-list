092b5ec5f50f1c72d6a54901affa509553b70d8a
kidus-93 <181005371+kidus-93@users.noreply.github.com>
2026-02-24 10:06:51 +0300

    Fibonacci Sequence

--- Number/Fibonacci Sequence.py
+++ Number/Fibonacci Sequence.py
@@ -1,13 +1,31 @@
-def fibonacci(n):
-    final = ""
-    values = []
-    for i in range(n+1):
-        fibonacci_value = round(((((1 + math.sqrt(5))/2)**i) - (((1 - math.sqrt(5))/2)**i))/math.sqrt(5))
-        values.append(fibonacci_value)
-        final = ", ".join(map(str, values))
-
-    print(final)
-
 import math
+
+## binet Formula
+
+# def fibonacci(n):
+#     final = ""
+#     values = []
+#     for i in range(n+1):
+#         fibonacci_value = round(((((1 + math.sqrt(5))/2)**i) - (((1 - math.sqrt(5))/2)**i))/math.sqrt(5))
+#         values.append(fibonacci_value)
+#
+#     final = ", ".join(map(str, values))
+#     print(final)
+#
+# import math
+# num = int(input("Please enter a number: "))
+# fibonacci(num)
+
+
+def fibonacci(n):
+    values = [0,1]
+    for i in range(2,n+1):
+        fibonacci_value = values[i-1] + values[i-2]
+        values.append(fibonacci_value)
+
+    print( ", ".join(map(str, values)))
+
+
+
 num = int(input("Please enter a number: "))
 fibonacci(num)
